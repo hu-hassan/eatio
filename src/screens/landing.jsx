12c301c378cb168d3cca2049caf30925378d7387
razatta <rizwan@devsurge.co>
2023-03-08 12:11:30 +0500

    design system basic usage

--- src/screens/landing.jsx
+++ src/screens/landing.jsx
@@ -1,9 +1,14 @@
 import { View, Text } from "react-native";
+import { colors, typography } from "../theme/designSystem";
 
 function Landing() {
   return (
-    <View style={{ flex: 1, backgroundColor: "red" }}>
-      <Text>han ma lnading sa hn</Text>
+    <View
+      style={{ flex: 1, backgroundColor: colors.secondary, marginTop: 100 }}
+    >
+      <Text style={typography.heading400}>hie</Text>
+      <Text style={typography.heading400}>hie again</Text>
+      <Text style={typography.slicedThrough}>hie again</Text>
     </View>
   );
 }
---
+++ src/theme/designSystem.js
@@ -0,0 +1,21 @@
+const colors = {
+  primary: "#DB3022",
+  secondary: "#EFC849",
+  accent: "#2AA952",
+};
+
+const typography = {
+  slicedThrough: {
+    fontWeight: 100,
+    textDecorationLine: "line-through",
+    fontStyle: "italic",
+    fontSize: 40,
+  },
+  heading400: {
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "blue",
+  },
+};
+
+export { colors, typography };
